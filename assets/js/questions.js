8e2ddec518e79c8aaea55c311eb09ea03030368e
Lowell Hallassou <lo@Lowells-Air.cust.communityfibre.co.uk>
2024-01-16 12:30:54 +0000

    Changing choices from list elements to button elements

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -10,14 +10,15 @@ let choicesEl = document.getElementById('choices')
 //Arrays of questions and answers
 let questionTitles = ['Where can you put JavaScript?', 'Which word is used to define a function in JavaScript?', 'JavaScript uses what kind of interface to access the DOM structure?','Which of these options does NOT require the use of parentheses?','Which of the following is not a valid method for generating output to the screen?','Every valid web page can be represented as a tree. This tree is referred to as the____','When a function returns a node from the DOM, it is of type______'];
 let choices = [
-['In the <head> section','In the <body> section','In an external file','All of the above'],
-['function','func','def','define function'],
-['CSS3','API','HTML5','IDE'],
-['console.log','prompt','innerHTML','alert'],
-['document.write', 'prompt','alert','print'],
-['Javascript','API','DOM','Linux'],
-['Object','Number','String','Boolean']
+['1. In the <head> section','2. In the <body> section','3. In an external file','4. All of the above'],
+['1. function','2. func','3. def','4. define function'],
+['1, CSS3','2. API','3. HTML5','4. IDE'],
+['1. console.log','2. prompt','3. innerHTML','4. alert'],
+['1. document.write', '2. prompt','3. alert','4. print'],
+['1. Javascript','2. API','3. DOM','4. Linux'],
+['1. Object','2. Number','3. String','4. Boolean']
 ];
+let correctAnswers = ['4. All of the above','1. function','2. API','3. inner.HTML','1. document.write','3. DOM','1. Object']
 
 //Function to switch to questions when start button is clicked
 function switchToQuestions(){
@@ -34,9 +35,18 @@ let selectedChoices = choices[randomIndex];
 //Displaying the questions and choices on the page
 questionEl.textContent = selectedQuestion;
 selectedChoices.forEach(function(choice){
-    let li = document.createElement('li');
-    li.textContent = choice;
-    choicesEl.appendChild(li);
+    let button = document.createElement('button');
+    button.textContent = choice;
+    button.addEventListener('click', resultOfChoice);
+    choicesEl.appendChild(button);
+
 });
 
-questionTitles.textContent = 'hi'
+function resultOfChoice(event) {
+    let selectedChoice = event.target.textContent;
+    
+    if (selectedChoice === correctAnswers) {
+
+    }
+}
+
