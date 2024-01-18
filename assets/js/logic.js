4a7ba82c5487cff1d0f1ce35338ce351f0786de5
Lowell Hallassou <lo@Lowells-MacBook-Air.local>
2024-01-18 12:08:29 +0000

    Making conditions for when quiz should end and load end screen

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -9,6 +9,7 @@ let secondsLeft = 75;
 //Adding event listener to start button for quiz timer to start
 startEl.addEventListener('click',function countdown() {
 
+
 //Counts down to 0 and ends quiz when time runs out
     let timerInterval = setInterval(function() {
     secondsLeft--;
@@ -21,6 +22,10 @@ startEl.addEventListener('click',function countdown() {
 }, 1000);
 
 })
+
+if (secondsLeft === 0) {
+    endQuiz();
+}
 
 
 
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -8,11 +8,11 @@ let choicesEl = document.getElementById('choices')
 
 
 //Arrays of questions and answers
-let questionTitles = ['Where can you put JavaScript?', 'Which word is used to define a function in JavaScript?', 'JavaScript uses what kind of interface to access the DOM structure?','Which of these options does NOT require the use of parentheses?','Which of the following is not a valid method for generating output to the screen?','Every valid web page can be represented as a tree. This tree is referred to as the____','When a function returns a node from the DOM, it is of type______'];
+let questionTitles = ['Where can you put JavaScript?', 'Which word is used to define a function in JavaScript?', 'JavaScript uses what kind of interface to access the DOM structure?','Which of these options does NOT require the use of parentheses?','Which of the following is not a valid method for generating output to the screen?','Every valid web page can be represented as a tree. This tree is referred to as the____.','When a function returns a node from the DOM, it is of type______.'];
 let choices = [
 ['1. In the <head> section','2. In the <body> section','3. In an external file','4. All of the above'],
 ['1. function','2. func','3. def','4. define function'],
-['1, CSS3','2. API','3. HTML5','4. IDE'],
+['1. CSS3','2. API','3. HTML5','4. IDE'],
 ['1. console.log','2. prompt','3. innerHTML','4. alert'],
 ['1. document.write', '2. prompt','3. alert','4. print'],
 ['1. Javascript','2. API','3. DOM','4. Linux'],
@@ -20,51 +20,59 @@ let choices = [
 ];
 let correctAnswers = ['4. All of the above','1. function','2. API','3. inner.HTML','1. document.write','3. DOM','1. Object']
 
+function loadQuestions() {
+    //Gaining random index to select questions and corresponding answers
+    let randomIndex = Math.floor(Math.random() * questionTitles.length);
+    
+    let selectedQuestion = questionTitles[randomIndex];
+    let selectedChoices = choices[randomIndex];
+    
+    //Displaying the questions and choices on the page
+    questionEl.textContent = selectedQuestion;
+    
+    selectedChoices.forEach(function(choice){
+        let button = document.createElement('button');
+        button.textContent = choice;
+        button.addEventListener('click', resultOfChoice);
+        choicesEl.appendChild(button);
+    
+    });
+    }
+
 //Function to switch to questions when start button is clicked
 function switchToQuestions(){
     startScreen.classList.toggle('hide');
     questions.classList.toggle('hide');
+    loadQuestions();
 }
 
-const loadQuestions = function() {
-//Gaining random index to select questions and corresponding answers
-let randomIndex = Math.floor(Math.random() * questionTitles.length);
-
-let selectedQuestion = questionTitles[randomIndex];
-let selectedChoices = choices[randomIndex];
-
-//Displaying the questions and choices on the page
-questionEl.textContent = selectedQuestion;
-
-selectedChoices.forEach(function(choice){
-    let button = document.createElement('button');
-    button.textContent = choice;
-    button.addEventListener('click', resultOfChoice);
-    choicesEl.appendChild(button);
-
-});
+function endQuiz() {
+    questions.classList.toggle('hide');
+    endScreen.classList.toggle('hide');
 }
 
-loadQuestions();
+let buttonPressCount = 0
 
 function resultOfChoice(event) {
+
+    buttonPressCount++;
+
     for (let i=0; i<questionTitles.length; i++) {
     let selectedChoice = event.target.textContent;
 
-    if (selectedChoice === correctAnswers) {
+    if (correctAnswers.includes(selectedChoice)) {
         choicesEl.innerHTML = ''
         loadQuestions();
     }
-    else if(selectedChoice !== correctAnswers) {
+    else if (!correctAnswers.includes(selectedChoice)) { 
         choicesEl.innerHTML = '';
         loadQuestions();
     }
-
     
+}
 
-    if (i >= questionTitles.length) {
-        endQuiz();
-    }
+if (buttonPressCount >= 7) {
+    endQuiz();
 }
 }
 
