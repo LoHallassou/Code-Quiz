16ed55f28f53d5dc23b3e09b90dfa2df0a013057
Lowell Hallassou <lo@Lowells-MacBook-Air.local>
2024-04-25 18:54:14 +0100

    Reverting back to older version

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,145 +1,30 @@
-let startScreen = document.getElementById('start-screen');
-let questions = document.getElementById('questions');
-let endScreen = document.getElementById('end-screen');
-
-let startBtn = document.querySelector('#start')
+let startEl = document.querySelector('#start')
 let timeEl = document.querySelector("#time");
-let questionsEl = document.querySelector('#question-title');
-let choicesEl = document.querySelector('choices');
-let initalsEl = document.querySelector('intials');
-let submitBtn = document.querySelector('submit');
-let feedbackEl = document.querySelector('feedback');
 
 
-timeEl.textContent = 7 * questionsChoices.length;
-let secondsLeft = 7 * questionsChoices.length;
-let timerInterval;
-let questionIndex = 0;
-
-function startQuiz () {
-
-    startScreen.classList.toggle('hide');
-    questions.classList.toggle('hide');
-
-    startBtn.addEventListener('click',function countdown() {
-
-    //Counts down to 0 and ends quiz when time runs out
-        timerInterval = setInterval(function() {
-        secondsLeft--;
-        timeEl.textContent = secondsLeft;
-        
-        if (secondsLeft <= 0) {
-            endQuiz();
-            }
-        }, 1000);
-        })
-    
-    loadQuestions();
-}
-
-// //Adding event listener to start button for quiz timer to start
-// startBtn.addEventListener('click',function countdown() {
+timeEl.textContent = 75;
+let secondsLeft = 75;
 
 
-// //Counts down to 0 and ends quiz when time runs out
-//     let timerInterval = setInterval(function() {
-//     secondsLeft--;
-//     timeEl.textContent = secondsLeft;
+//Adding event listener to start button for quiz timer to start
+startEl.addEventListener('click',function countdown() {
 
-//     if (secondsLeft === 0) {
-//         clearInterval(timerInterval);
-//         endQuiz();
-//     }
-// }, 1000);
 
-// })
+//Counts down to 0 and ends quiz when time runs out
+    let timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
 
-// if (secondsLeft === 0) {
-//     endQuiz();
-// }
-
-//Gaining random index to select questions and corresponding answers
-// function randomIndex() {
-//     return Math.floor(Math.random() * questionTitles.length);
-// }
-
-function loadQuestions() {
-
-    let quizQuestion = questionsChoices[questionIndex];
-    questionEl.textContent = quizQuestion.question;
-    
-    choicesEl.innerHTML = ''
-
-   quizQuestion.options.forEach(function(choice){
-        let optionButton = document.createElement('button');
-        optionButton.setAttribute('class','options')
-        optionButton.setAttribute('value', choice);
-
-        optionButton.textContent = choice;
-
-        optionButton.addEventListener('click', resultOfChoice);
-
-        choicesEl.appendChild(optionButton);
-        
-    });
+    if (secondsLeft === 0) {
+        clearInterval(timerInterval);
+        endQuiz();
     }
+}, 1000);
 
-//Function to switch to questions when start button is clicked
-// function switchToQuestions(){
-//     startScreen.classList.toggle('hide');
-//     questions.classList.toggle('hide');
-//     loadQuestions();
-// }
+})
 
-function resultOfChoice() {
-
-if (this.value === questions[questionIndex].answer) {   
-    timerEl.textContent = secondsLeft;
-    feedbackEl.textContent = "Correct!";
-    feedbackEl.style.fontSize = "24px"
-    feedbackEl.style.color = "green";
-}
-
-else {
-    secondsLeft -= 5;
-    if (secondsLeft < 0) {
-      secondsLeft = 0;
-    }
-    feedbackEl.textContent = "Wrong!";
-    feedbackEl.style.fontSize = "24px"
-    feedbackEl.style.color = "red";
-}
-
-
-feedbackEl.setAttribute("class", "feedback");
-  setTimeout(function() {
-    feedbackEl.setAttribute("class", "feedback hide");
-  }, 1000);
-    
-  questionIndex++;
-   
-  if (questionIndex === questionsChoices.length) {
+if (secondsLeft === 0) {
     endQuiz();
-  } else {
-    loadQuestions();
-  }     
-
 }
-
-
-let buttonPressCount = 0
-
-function endQuiz() {
-
-    questions.classList.toggle('hide');
-    endScreen.classList.toggle('hide');
-
-    let fScore = document.getElementById('final-score');
-    fScore.textContent = secondsLeft;
-
-    clearInterval(timerInterval);
-}
-
-
 
 
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -8,60 +8,35 @@ let choicesEl = document.getElementById('choices')
 
 
 //Arrays of questions and answers
+let questionTitles = ['Where can you put JavaScript?', 'Which word is used to define a function in JavaScript?', 'JavaScript uses what kind of interface to access the DOM structure?','Which of these options does NOT require the use of parentheses?','Which of the following is not a valid method for generating output to the screen?','Every valid web page can be represented as a tree. This tree is referred to as the____.','When a function returns a node from the DOM, it is of type______.'];
+let choices = [
+['1. In the <head> section','2. In the <body> section','3. In an external file','4. All of the above'],
+['1. function','2. func','3. def','4. define function'],
+['1. CSS3','2. API','3. HTML5','4. IDE'],
+['1. console.log','2. prompt','3. innerHTML','4. alert'],
+['1. document.write', '2. prompt','3. alert','4. print'],
+['1. Javascript','2. RAM','3. DOM','4. Linux'],
+['1. Object','2. Number','3. String','4. Boolean']
+];
+let correctAnswers = ['4. All of the above','1. function','2. API','3. inner.HTML','1. document.write','3. DOM','1. Object']
 
 
-let answeredIndex = [];
-
-let questionsChoices = [{
-    question: 'Where can you put JavaScript?',
-    options: ['1. In the <head> section','2. In the <body> section','3. In an external file','4. All of the above'],
-    answer: "4. All of the above"
-},
-{
-    question: 'Which word is used to define a function in JavaScript?',
-    options: ['1. function','2. func','3. def','4. define function'],
-    answer: '1. function'
-},
-{
-    question:'JavaScript uses what kind of interface to access the DOM structure?',
-    options: ['1. CSS3','2. API','3. HTML5','4. IDE'],
-    answer: '2. API'
-},
-{
-    questions: 'Which of these options does NOT require the use of parentheses?',
-    options: ['1. console.log','2. prompt','3. innerHTML','4. alert'],
-    answer: '3. inner.HTML'
-},
-{
-    questions: 'Which of the following is not a valid method for generating output to the screen?',
-    options: ['1. document.write', '2. prompt','3. alert','4. print'],
-    answer: '1. document.write',   
-},
-{
-    questions: 'Every valid web page can be represented as a tree. This tree is referred to as the____.',
-    options: ['1. Javascript','2. RAM','3. DOM','4. Linux'],
-    answer: '3. DOM',
-},
-{
-    questions: 'When a function returns a node from the DOM, it is of type______.',
-    options: ['1. Object','2. Number','3. String','4. Boolean'],
-    answer: '1. Object'
-}
-]
 
 //Gaining random index to select questions and corresponding answers
-function randomIndex() {
-    return Math.floor(Math.random() * questionTitles.length);
-}
+let randomIndex = Math.floor(Math.random() * questionTitles.length);
 
 function loadQuestions() {
 
-    let quizQuestions = questionTitles.slice();
-    let selectedQuestion = quizQuestions[randomIndex()];
+    let randomIndex = Math.floor(Math.random() * questionTitles.length);
 
+    let quizQuestions = questionTitles.slice();
     let quizChoices = choices.slice();
-    let selectedChoices = quizChoices[randomIndex()];
+
+    let selectedQuestion = quizQuestions[randomIndex];
+    let selectedChoices = quizChoices[randomIndex];
     
+    quizQuestions.splice(randomIndex, 1);
+    quizChoices.splice(randomIndex, 1);
     
     //Displaying the questions and choices on the page
     questionEl.textContent = selectedQuestion;
@@ -72,7 +47,7 @@ function loadQuestions() {
         button.addEventListener('click', resultOfChoice);
         choicesEl.appendChild(button);
 
-        console.log(answeredIndex);
+    
     });
     }
 
@@ -93,12 +68,6 @@ let buttonPressCount = 0
 function resultOfChoice(event) {
 
     buttonPressCount++;
-    if (answeredIndex.includes(randomIndex())) {
-        randomIndex();
-    }
-    else {
-        answeredIndex.push(randomIndex());
-    }
 
     for (let i=0; i<questionTitles.length; i++) {
     let selectedChoice = event.target.textContent;
@@ -109,12 +78,6 @@ function resultOfChoice(event) {
     }
     else if (!correctAnswers.includes(selectedChoice)) { 
         choicesEl.innerHTML = '';
-        if (answeredIndex.includes(randomIndex()) === false) {
-            answeredIndex.push(randomIndex());
-        }
-        else {
-            randomIndex();
-        }
         loadQuestions();
     }
     
@@ -124,4 +87,3 @@ if (buttonPressCount >= 7) {
     endQuiz();
 }
 }
-
